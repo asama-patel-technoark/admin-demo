0f9d97d01023abdd8686650bc10d3f22f0cc2dc9
Asama Patel <asama.patel@technoark.io>
2022-01-29 18:10:08 +0530

    TrelloBoard working

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,102 +1,102 @@
-import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import Logo from "../assets/images/logo-black.png";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import * as React from 'react'
+import { styled, useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import CssBaseline from '@mui/material/CssBaseline'
+import MuiAppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import MailIcon from '@mui/icons-material/Mail'
+import Logo from '../assets/images/logo-black.png'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: `-${drawerWidth}px`,
     ...(open && {
-      transition: theme.transitions.create("margin", {
+      transition: theme.transitions.create('margin', {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       marginLeft: 0,
     }),
-  })
-);
+  }),
+)
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  backgroundColor: "white",
-  color: "black",
-  transition: theme.transitions.create(["margin", "width"], {
+  backgroundColor: 'white',
+  color: 'black',
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
-}));
+}))
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
-}));
+  justifyContent: 'flex-end',
+}))
 
 export default function PersistentDrawerLeft(props) {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-  let navigate = useNavigate();
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+  let navigate = useNavigate()
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
+      <AppBar position='fixed' open={open}>
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
+            color='inherit'
+            aria-label='open drawer'
             onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: "none" }) }}
+            edge='start'
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant='h6' noWrap component='div'>
             Admin Frontend
           </Typography>
         </Toolbar>
@@ -105,19 +105,19 @@ export default function PersistentDrawerLeft(props) {
         sx={{
           width: drawerWidth,
           flexShrink: 0,
-          "& .MuiDrawer-paper": {
+          '& .MuiDrawer-paper': {
             width: drawerWidth,
-            boxSizing: "border-box",
+            boxSizing: 'border-box',
           },
         }}
-        variant="persistent"
-        anchor="left"
+        variant='persistent'
+        anchor='left'
         open={open}
       >
         <DrawerHeader>
-          <img src={Logo} height="60" />
+          <img src={Logo} height='60' />
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
+            {theme.direction === 'ltr' ? (
               <ChevronLeftIcon />
             ) : (
               <ChevronRightIcon />
@@ -130,30 +130,42 @@ export default function PersistentDrawerLeft(props) {
             button
             // key={text}
             onClick={() => {
-              navigate("/dashboard", { replace: true });
+              navigate('/dashboard', { replace: true })
             }}
           >
             <ListItemIcon>
               <InboxIcon />
             </ListItemIcon>
-            <ListItemText primary={"Dashboard"} />
+            <ListItemText primary={'Dashboard'} />
           </ListItem>
           <ListItem
             button
             // key={text}
             onClick={() => {
-              navigate("/", { replace: true });
+              navigate('/', { replace: true })
             }}
           >
             <ListItemIcon>
               <InboxIcon />
             </ListItemIcon>
-            <ListItemText primary={"Login"} />
+            <ListItemText primary={'Login'} />
+          </ListItem>
+          <ListItem
+            button
+            // key={text}
+            onClick={() => {
+              navigate('/trelloboard', { replace: true })
+            }}
+          >
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={'Trello Board'} />
           </ListItem>
         </List>
         <Divider />
         <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -163,7 +175,7 @@ export default function PersistentDrawerLeft(props) {
           ))}
         </List>
       </Drawer>
-      <Main open={open} style={{ backgroundColor: "#f4f6fa", height: "100vh" }}>
+      <Main open={open} style={{ backgroundColor: '#f4f6fa', height: '100vh' }}>
         <DrawerHeader />
         <Outlet />
         {/* <Typography paragraph>
@@ -197,5 +209,5 @@ export default function PersistentDrawerLeft(props) {
         </Typography> */}
       </Main>
     </Box>
-  );
+  )
 }
--- src/lib/Routes.js
+++ src/lib/Routes.js
@@ -1,28 +1,30 @@
-import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import Login from "../pages/auth/Login";
-import Dashboard from "../pages/Dashboard";
-import Topbar from "../layouts/Topbar";
-import Sidebar from "../layouts/Sidebar";
-import AuthLayout from "../layouts/AuthLayout.";
-import "../assets/styles/Layout1.css";
+import * as React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Login from '../pages/auth/Login'
+import Dashboard from '../pages/Dashboard'
+import Topbar from '../layouts/Topbar'
+import Sidebar from '../layouts/Sidebar'
+import AuthLayout from '../layouts/AuthLayout.'
+import '../assets/styles/Layout1.css'
+import TrelloBoard from '../pages/TrelloBoard'
 
 export default function AppRoutes() {
   return (
     <>
       {/* <Topbar /> */}
       <Routes>
-        <Route path={"/"} element={<AuthLayout />}>
+        <Route path={'/'} element={<AuthLayout />}>
           {/* <Redirect exact from="/" to="/login" /> */}
-          <Route path="/login" element={<Login />} />
+          <Route path='/login' element={<Login />} />
         </Route>
         <Route element={<Sidebar />}>
           {/* <Sidebar> */}
           {/* <Route path="/" element={<Login />} /> */}
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/trelloboard' element={<TrelloBoard />} />
           {/* </Sidebar> */}
         </Route>
       </Routes>
     </>
-  );
+  )
 }
